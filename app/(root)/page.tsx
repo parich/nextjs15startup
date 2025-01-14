40f4f60144e04a2ce3ddb1279fc1e67f24a091cb
parich suriya <parich.suriya@gmail.com>
2025-01-14 09:54:45 +0700

    Hero Section

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <>HOME</>;
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup, <br /> Connect With Entrepreners
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, vote on Pitch, and Get Noticed in Virtual Competitions.
+        </p>
+      </section>
+    </>
+  );
 }
